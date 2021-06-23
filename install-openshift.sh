9d8f3fe8179b12260d28d26d17cb4397c2cf6c77
Svetozar Gremyachikh <sgremyachikh@gmail.com>
2021-06-23 12:15:44 +0300

    initial commit. install-openshift.sh is updated. added hack of synk.yml in RH repo. vol.yml is updated.

--- install-openshift.sh
+++ install-openshift.sh
@@ -5,32 +5,45 @@
 ## Default variables to use
 export INTERACTIVE=${INTERACTIVE:="true"}
 export PVS=${PVS:="true"}
-export DOMAIN=${DOMAIN:="$(curl -s ipinfo.io/ip).nip.io"}
-export USERNAME=${USERNAME:="$(whoami)"}
-export PASSWORD=${PASSWORD:=password}
+export DOMAIN=${DOMAIN:="my.wildcard.domain"}
+
+export USERNAME=${USERNAME:="openshift_admin_login"}
+export PASSWORD=${PASSWORD:="openshift_admin_password"}
+export ADMIN_USERNAME=${ADMIN_USERNAME:="marfa"}
+export ADMIN_PASSWORD=${ADMIN_PASSWORD:="Fie0za6n"}
+
 export VERSION=${VERSION:="3.11"}
 export SCRIPT_REPO=${SCRIPT_REPO:="https://raw.githubusercontent.com/okd-community-install/installcentos/master"}
 export IP=${IP:="$(ip route get 8.8.8.8 | awk '{print $NF; exit}')"}
 export API_PORT=${API_PORT:="8443"}
+
+export LOGGING=${LOGGING:="False"}
+export METRICS=${METRICS:="False"}
+
 export LETSENCRYPT=${LETSENCRYPT:="false"}
 export CLOUDFLARE=${CLOUDFLARE:="false"}
 export CF_MAIL=${CF_MAIL:="example@email.com"}
 export CF_KEY=${CF_KEY:="xxxxxx"}
 export MAIL=${MAIL:="example@email.com"}
 
+export DEPLOY_PREPEARED_LETSENCRYPT=${DEPLOY_PREPEARED_LETSENCRYPT:="False"}
+export PREPEARED_LETSENCRYPT_CERTFILE=${PREPEARED_LETSENCRYPT_CERTFILE:="/etc/letsencrypt/live/${DOMAIN}/fullchain.pem"}
+export PREPEARED_LETSENCRYPT_KEYFILE=${PREPEARED_LETSENCRYPT_KEYFILE:="/etc/letsencrypt/live/${DOMAIN}/privkey.pem"}
+export PREPEARED_LETSENCRYPT_CAFILE=${PREPEARED_LETSENCRYPT_CAFILE:="/etc/letsencrypt/live/${DOMAIN}/chain.pem"}
+
 ## Make the script interactive to set the variables
 if [ "$INTERACTIVE" = "true" ]; then
-	read -rp "Domain to use: ($DOMAIN): " choice;
+	read -rp "Wildcard Domain to use: ($DOMAIN): " choice;
 	if [ "$choice" != "" ] ; then
 		export DOMAIN="$choice";
 	fi
 
-	read -rp "Username: ($USERNAME): " choice;
+	read -rp "OpenShift admin Username: ($USERNAME): " choice;
 	if [ "$choice" != "" ] ; then
 		export USERNAME="$choice";
 	fi
 
-	read -rp "Password: ($PASSWORD): " choice;
+	read -rp "OpenShift admin Password: ($PASSWORD): " choice;
 	if [ "$choice" != "" ] ; then
 		export PASSWORD="$choice";
 	fi
@@ -39,7 +52,7 @@ if [ "$INTERACTIVE" = "true" ]; then
 	if [ "$choice" != "" ] ; then
 		export VERSION="$choice";
 	fi
-	read -rp "IP: ($IP): " choice;
+	read -rp "Single node cluster host IP: ($IP): " choice;
 	if [ "$choice" != "" ] ; then
 		export IP="$choice";
 	fi
@@ -108,17 +121,17 @@ echo "******"
 # install updates
 yum update -y
 
+#install epel
+yum -y install epel-release
+
 # install the following base packages
-yum install -y  wget git zile nano net-tools docker-1.13.1\
-				bind-utils iptables-services \
+yum install -y  wget git vim tmux zile nano net-tools docker-1.13.1\
+				bind-utils iptables-services ansible pyOpenSSL \
 				bridge-utils bash-completion \
 				kexec-tools sos psacct openssl-devel \
 				httpd-tools NetworkManager \
 				python-cryptography python2-pip python-devel  python-passlib \
 				java-1.8.0-openjdk-headless "@Development Tools"
-
-#install epel
-yum -y install epel-release
 
 # Disable the EPEL repository globally so that is not accidentally used during later steps of the installation
 sed -i -e "s/^enabled=1/enabled=0/" /etc/yum.repos.d/epel.repo
@@ -129,13 +142,17 @@ if [ $? -eq 1 ]; then
 	systemctl enable NetworkManager
 fi
 
-# install the packages for Ansible
-yum -y --enablerepo=epel install pyOpenSSL
+# # install the packages for Ansible
+# yum -y --enablerepo=epel install pyOpenSSL
 
-curl -o ansible.rpm https://releases.ansible.com/ansible/rpm/release/epel-7-x86_64/ansible-2.6.5-1.el7.ans.noarch.rpm
-yum -y --enablerepo=epel install ansible.rpm
+# curl -o ansible.rpm https://releases.ansible.com/ansible/rpm/release/epel-7-x86_64/ansible-2.6.5-1.el7.ans.noarch.rpm
+# yum -y --enablerepo=epel install ansible.rpm
 
 [ ! -d openshift-ansible ] && git clone https://github.com/openshift/openshift-ansible.git -b release-${VERSION} --depth=1
+
+# replacing sync.yml with "--insecure" flag to success curl of api server with self-signet certs
+rm -f ./openshift-ansible/playbooks/common/openshift-cluster/roles/openshift_node_group/tasks/sync.yml
+cp sync.yml ./openshift-ansible/playbooks/common/openshift-cluster/roles/openshift_node_group/tasks/sync.yml
 
 cat <<EOD > /etc/hosts
 127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4
@@ -168,9 +185,6 @@ if [ ! -f ~/.ssh/id_rsa ]; then
 	cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 	ssh -o StrictHostKeyChecking=no root@$IP "pwd" < /dev/null
 fi
-
-export METRICS="True"
-export LOGGING="True"
 
 memory=$(cat /proc/meminfo | grep MemTotal | sed "s/MemTotal:[ ]*\([0-9]*\) kB/\1/")
 
@@ -269,8 +283,38 @@ EOT
 	rm certbotcron
 fi
 
+# Let's Encrypt created certs deploy setup
+if [ "$DEPLOY_PREPEARED_LETSENCRYPT" = true ] ;
+then
+
+	## Modify inventory.ini
+	# Declare usage of Custom Certificate
+	# Configure Custom Certificates for the Web Console or CLI => Doesn't Work for CLI
+	# Configure a Custom Master Host Certificate
+	# Configure a Custom Wildcard Certificate for the Default Router
+	# Configure a Custom Certificate for the Image Registry
+	## See here for more explanation: https://docs.okd.io/latest/install_config/certificate_customization.html
+	cat <<EOT >> inventory.ini
+
+	openshift_master_overwrite_named_certificates=true
+
+	openshift_master_cluster_hostname=console-internal.${DOMAIN}
+	openshift_master_cluster_public_hostname=console.${DOMAIN}
+
+	openshift_master_named_certificates=[{"certfile": "${PREPEARED_LETSENCRYPT_CERTFILE}", "keyfile": "${PREPEARED_LETSENCRYPT_KEYFILE}", "cafile": "${PREPEARED_LETSENCRYPT_CAFILE}", "names": ["console.${DOMAIN}"]}]
+
+	openshift_hosted_router_certificate={"certfile": "${PREPEARED_LETSENCRYPT_CERTFILE}", "keyfile": "${PREPEARED_LETSENCRYPT_KEYFILE}", "cafile": "${PREPEARED_LETSENCRYPT_CAFILE}"}
+
+	openshift_hosted_registry_routehost=registry.apps.${DOMAIN}
+	openshift_hosted_registry_routecertificates={"certfile": "${PREPEARED_LETSENCRYPT_CERTFILE}", "keyfile": "${PREPEARED_LETSENCRYPT_KEYFILE}", "cafile": "${PREPEARED_LETSENCRYPT_CAFILE}"}
+	openshift_hosted_registry_routetermination=reencrypt
+EOT
+
+fi
+
 mkdir -p /etc/rhsm/ca
 openssl s_client -showcerts -servername registry.access.redhat.com -connect registry.access.redhat.com:443 </dev/null 2>/dev/null | openssl x509 -text > /etc/rhsm/ca/redhat-uep.pem
+
 
 mkdir -p /etc/origin/master/
 touch /etc/origin/master/htpasswd
@@ -281,11 +325,14 @@ ansible-playbook -i inventory.ini openshift-ansible/playbooks/deploy_cluster.yml
 htpasswd -b /etc/origin/master/htpasswd ${USERNAME} ${PASSWORD}
 oc adm policy add-cluster-role-to-user cluster-admin ${USERNAME}
 
+htpasswd -b /etc/origin/master/htpasswd ${ADMIN_USERNAME} ${ADMIN_PASSWORD}
+oc adm policy add-cluster-role-to-user cluster-admin ${ADMIN_USERNAME}
+
 if [ "$PVS" = "true" ]; then
 
 	curl -o vol.yaml $SCRIPT_REPO/vol.yaml
 
-	for i in `seq 1 200`;
+	for i in `seq 1 300`;
 	do
 		DIRNAME="vol$i"
 		mkdir -p /mnt/data/$DIRNAME
@@ -307,7 +354,7 @@ echo "* Your password is $PASSWORD "
 echo "*"
 echo "* Login using:"
 echo "*"
-echo "$ oc login -u ${USERNAME} -p ${PASSWORD} https://console.$DOMAIN:$API_PORT/"
+echo "$ oc login -u ${USERNAME} -p ${PASSWORD} https://console.$DOMAIN:$API_PORT/ --insecure-skip-tls-verify"
 echo "******"
 
 oc login -u ${USERNAME} -p ${PASSWORD} https://console.$DOMAIN:$API_PORT/
